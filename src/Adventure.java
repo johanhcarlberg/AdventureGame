38f0f737414c801555a7e16a44b60c8c1d779f81
Johan Hansson Carlberg <johan.hansson.carlberg@gmail.com>
2023-11-07 18:12:19 +0100

    Add constructor and method for selecting type of adventure

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,3 +1,23 @@
-public class Adventure {
+import java.util.Random;
 
+public class Adventure {
+    private World world;
+    private Random random;
+    private final static int DILEMMA_CHANCE = 15;
+
+    public Adventure(World world) {
+        this.world = world;
+        this.random = new Random();
+    }
+
+    public void selectAdventureType() {
+        int randNum = random.nextInt(100) + 1;
+        if (randNum <= DILEMMA_CHANCE) {
+            // Adventure type is dilemma
+            // TODO: Select a dilemma from world
+        } else {
+            // Adventure type is battle
+            // TODO: Select a monster from world
+        }
+    }
 }
