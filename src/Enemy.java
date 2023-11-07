6d16a1071f887d8cbe46d15b71ae3e93560adc11
tomanstensen <tom.anstensen@iths.se>
2023-11-07 10:48:16 +0100

    Fiendeklass skapad

---
+++ src/Enemy.java
@@ -0,0 +1,34 @@
+public class Enemy {
+    private String name;
+    private int maxHealth;
+    private int currentHealth;
+    private int egenskapSty;
+    private int egenskapSmi;
+
+    public Enemy(String name, int maxHealth, int egenskapSty, int egenskapSmi){
+        this.name=name;
+        this.maxHealth=maxHealth;
+        this.egenskapSty=egenskapSty;
+        this.egenskapSmi=egenskapSmi;
+    }
+
+    
+
+
+    public String getName() {
+        return getName();
+    }
+
+    public int getHealth() {
+        return getHealth();
+    }
+
+    public int getEgenskapSty() {
+        return getEgenskapSty();
+    }
+
+    public int getEgenskapSmi() {
+        return getEgenskapSmi();
+    }
+}
+
