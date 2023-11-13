daaadb3342fea17c19a0c5e8b1d8a4862ad530cc
Emelie Pozzi <emelie.pozzi@iths.se>
2023-11-13 10:05:09 +0100

    Flyttat in värdshusmenyn in i Game

--- src/Game.java
+++ src/Game.java
@@ -2,14 +2,64 @@ import java.util.Scanner;
 
 public class Game {
     public String playerName;
+    Scanner sc = new Scanner(System.in);
 
     public void startGame() {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Welcome to the Magic House!");
-            System.out.println("What is your characters name? ");
-            playerName = sc.nextLine();
-        }
-        System.out.println("Hello " + playerName + "! Welcome to the game! ");
         
+        System.out.println("Welcome to the Magic House!");
+        System.out.println("What is your characters name? ");
+        playerName = sc.nextLine();
+        System.out.println("Hello " + playerName + "! Welcome to the game! ");
+            
+    }
+        
+    //Metod som visar VärdshusMenyn.
+    public void showMenuAlternativesLoop(){
+           
+         while (true){ //Loopen körs tills ett break kommer.
+            showInnAlternatives();
+            int userInputChoiceInMenu = sc.nextInt(); 
+            System.out.println("Du valde alternativ nummer: " + userInputChoiceInMenu);
+
+            if (userInputChoiceInMenu == 1){
+                goOnAdventure();
+                break; //Avslutar loopen om ett giltigt val görs.
+            }
+            else if (userInputChoiceInMenu ==2){
+                viewCharacterSheet();
+                break; 
+            }
+            else if (userInputChoiceInMenu == 3){
+                restCharacter();
+                break; 
+            }
+            else {
+                System.out.println("Du har angivit ett felaktiskt alternativ. Försök igen.");
+                    
+            }
+
+                
+        }
+    }
+    
+       
+
+    public void showInnAlternatives() {
+        System.out.println("Du är nu i värdshuset. Ange siffran som hör till ditt val.");
+        System.out.println("1: Gå på äventyr");
+        System.out.println("2: Granska dina egenskaper");
+        System.out.println("3: Vila och återställ din hälsa");
+    }
+
+    public void goOnAdventure() {
+        //Gå på äventyr
+    }
+    
+    public void viewCharacterSheet() {
+        //Visa karaktärs egenskaper
+    }
+    
+    public void restCharacter() {
+        //Vila upp karaktären
     }
 }
