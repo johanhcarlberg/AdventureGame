3572d527057d65a940a18710505d154da7fabc45
Johan Carlberg <johan.hansson.carlberg@gmail.com>
2023-11-13 09:07:05 +0100

    Change DILEMMA_CHANCE to DILEMMA_CHANCE_PERCENTAGE for clarity

--- src/Adventure.java
+++ src/Adventure.java
@@ -8,7 +8,7 @@ enum EncounterType {
 public class Adventure {
     private World world;
     private Random random;
-    private final static int DILEMMA_CHANCE = 15;
+    private final static int DILEMMA_CHANCE_PERCENTAGE = 15;
 
     public Adventure(World world) {
         this.world = world;
