b187752e8066f8c70994073ba45cd1afcf23ec0e
Johan Hansson Carlberg <johan.hansson.carlberg@gmail.com>
2023-11-16 12:41:51 +0100

    Create initial structure for CharacterClass

--- src/CharacterClass.java
+++ src/CharacterClass.java
@@ -1,3 +1,35 @@
 public class CharacterClass {
-    
+    private String name;
+    private int initialStrength;
+    private int strengthPerLevel;
+    private int initialDexterity;
+    private int dexterityPerLevel;
+
+    public CharacterClass(String name, int initialStrength, int strengthPerLevel, int initialDexterity, int dexterityPerLevel) {
+        this.name = name;
+        this.initialStrength = initialStrength;
+        this.strengthPerLevel = strengthPerLevel;
+        this.initialDexterity = initialDexterity;
+        this.dexterityPerLevel = dexterityPerLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getInitialStrength() {
+        return initialStrength;
+    }
+
+    public int getStrengthPerLevel() {
+        return strengthPerLevel;
+    }
+
+    public int getInitialDexterity() {
+        return initialDexterity;
+    }
+
+    public int getDexterityPerLevel() {
+        return dexterityPerLevel;
+    }
 }
