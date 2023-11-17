c25b4aa6a2c1768d98cc2391749e0e77106bca09
Johan Hansson Carlberg <johan.hansson.carlberg@gmail.com>
2023-11-17 13:48:28 +0100

    Set currentWorld to water world in setupGame

--- src/Game.java
+++ src/Game.java
@@ -59,11 +59,14 @@ public class Game {
         CharacterClass.availableClasses.add(new CharacterClass("Rogue", 3, 2, 5, 3));
 
         World waterWorld = new World("Water");
+        
         EnemyTypes seaHorseType = new EnemyTypes("Seahorse", 10, 3, 4, 7);
         waterWorld.getEnemies().add(seaHorseType.create(2));
         // Skapa alla fiendetyper
         // Skapa alla världar
         // Lägg till fienderna i rätt värld
+
+        currentWorld = waterWorld; // Spelaren börjar i vattenvärlden.
     }
         
     //Metod som visar VärdshusMenyn.
