aa78502684ad6726c89e5200370cc8e5c3f651f0
Josefin-K <josefinkarlsson@protonmail.com>
2023-11-17 13:21:00 +0100

    Ändrat till Dilemma och EnemyTypes

--- src/World.java
+++ src/World.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class World {
     private String theme;
-    private List<Enemy> enemies;
-    private List<String> dilemmas;
+    private List<EnemyTypes> enemies;
+    private List<Dilemma> dilemmas;
 
     public World(String theme) {
         this.theme = theme;
@@ -19,19 +19,19 @@ public class World {
         this.theme = theme;
     }
 
-    public List<Enemy> getEnemies() {
+    public List<EnemyTypes> getEnemies() {
         return enemies;
     }
 
-    public void setEnemies(List<Enemy> enemies) {
+    public void setEnemies(List<EnemyTypes> enemies) {
         this.enemies = enemies;
     }
 
-    public List<String> getDilemmas() {
+    public List<Dilemma> getDilemmas() {
         return dilemmas;
     }
 
-    public void setDilemmas(List<String> dilemmas) {
+    public void setDilemmas(List<Dilemma> dilemmas) {
         this.dilemmas = dilemmas;
     }
 }
