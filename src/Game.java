1cbf7b9c6bef60943a0868fad7a5b40705d153fc
Johan Hansson Carlberg <johan.hansson.carlberg@gmail.com>
2023-11-17 13:48:46 +0100

    Change currentAdventure to use currentWorld

--- src/Game.java
+++ src/Game.java
@@ -109,8 +109,7 @@ public class Game {
 
     public void goOnAdventure() {
         //Gå på äventyr
-        World world = new World("Volcano");
-        currentAdventure = new Adventure(new World("Volcano"), player);
+        currentAdventure = new Adventure(currentWorld, player);
         currentGameState = GameState.ADVENTURE;
     }
     
