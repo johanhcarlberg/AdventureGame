35ede50cf8d1a6c6b5a6a60e8e80f1dc19ee43fe
Johan Hansson Carlberg <johan.hansson.carlberg@gmail.com>
2023-11-20 11:16:00 +0100

    Create character class

---
+++ src/Character.java
@@ -0,0 +1,65 @@
+public class Character {
+    private String name;
+    private int level;
+    private int maxHealth;
+    private int currentHealth;
+    private int strength;
+    private int dexterity;
+
+    public Character(String name, int level, int maxHealth, int currentHealth, int strength, int dexterity) {
+        this.name = name;
+        this.level = level;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+        this.strength = strength;
+        this.dexterity = dexterity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    protected void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    protected void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    protected void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    protected void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+    
+    protected void setStrength(int strength) {
+        this.strength = strength;
+    }
+}
