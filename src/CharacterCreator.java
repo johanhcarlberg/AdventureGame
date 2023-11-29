7a5a050b87c664a865858b149a2301f0027e8830
Johan Carlberg <johan.hansson.carlberg@gmail.com>
2023-11-29 11:10:25 +0100

    Add character name validation

--- src/CharacterCreator.java
+++ src/CharacterCreator.java
@@ -1,9 +1,17 @@
+import java.lang.Character;
+
 public class CharacterCreator {
+    private final int NAME_MAX_LENGTH = 15;
+
     public Player createCharacter() {
         System.out.println("As you walk up to the city gate, a guard approaches.");
         System.out.println("\"So you want to be an adventurer, eh? What's your name?\"");
-        System.out.print("Character name: ");
-        String playerName = Input.getString();
+        
+        String playerName;
+        do {
+            System.out.print("Character name: ");
+            playerName = Input.getString();
+        } while(!validateCharacterName(playerName));
 
         CharacterClass playerClass = selectClass();
 
@@ -29,6 +37,35 @@ public class CharacterCreator {
         CharacterClass selectedClass = characterClasses[classSelection - 1];
 
         return selectedClass;
+    }
+
+    private boolean validateCharacterName(String name) {
+        if(name.length() > NAME_MAX_LENGTH) {
+            System.out.println("Character name must be 15 characters or less.");
+            return false;
+        }
+
+        if(name.isEmpty()) {
+            System.out.println("Character name cannot be empty.");
+            return false;
+        }
+
+        if(!isAlphanumeric(name)) {
+            System.out.println("Character name must be alphanumeric.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphanumeric(String name) {
+        for (char c : name.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 
