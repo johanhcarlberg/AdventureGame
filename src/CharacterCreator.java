333e56ebab0bd977f6c6817c871c81cddec60290
Johan Carlberg <johan.hansson.carlberg@gmail.com>
2023-11-29 11:01:34 +0100

    Create CharacterCreator class, move createCharacter and selectClass methods from Game

---
+++ src/CharacterCreator.java
@@ -0,0 +1,35 @@
+public class CharacterCreator {
+    public Player createCharacter() {
+        System.out.println("As you walk up to the city gate, a guard approaches.");
+        System.out.println("\"So you want to be an adventurer, eh? What's your name?\"");
+        System.out.print("Character name: ");
+        String playerName = Input.getString();
+
+        CharacterClass playerClass = selectClass();
+
+        Player player = new Player(playerName, playerClass);
+        String characterIntroString = String.format("\'I'm %s, a %s! And I'm gonna be the greatest adventurer ever!\'", player.getName(), player.getPlayerClass().getName());
+        System.out.println(characterIntroString);
+        System.out.println("\"Well good luck then... I used to be an adventurer like you, until I took an arrow to the knee. Now I'm just a guard in this town.\""); 
+        System.out.println("\"Head on in. There is an inn called The Wasted Wanderer where you can find some tips on adventuring!\"\n");
+
+        return player;
+    }
+
+    public CharacterClass selectClass() {
+        CharacterClass[] characterClasses = new CharacterClass[CharacterClass.availableClasses.size()];
+        CharacterClass.availableClasses.toArray(characterClasses);
+
+        System.out.println("Select your class:");
+        for (int i = 0; i < characterClasses.length; i++) {
+            System.out.println(i + 1 + ". " + characterClasses[i].getName());
+        }
+
+        int classSelection = Input.getIntegerInRange(1, characterClasses.length);
+        CharacterClass selectedClass = characterClasses[classSelection - 1];
+
+        return selectedClass;
+    }
+
+
+}
